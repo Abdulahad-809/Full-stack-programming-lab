43ca1a3befc8b82bf70c7287bc016423ba62f5ad
Abdulahad-809 <ahad.2582004@gmail.com>
2026-05-11 23:08:55 +0500

    assignment 2
    Assignment 2 uploaded

--- Lab_07_hottube-react-app/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the category page', () => {
-  window.history.pushState({}, 'Category', '/category');
-  render(<App />);
-  expect(screen.getByRole('heading', { name: /top product listing/i })).toBeInTheDocument();
-  expect(screen.getByRole('searchbox', { name: /search/i })).toBeInTheDocument();
-});
